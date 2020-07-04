0a34b6498ce26e369b42beafd4e2d380f23ac78e
sanyam-jn <55603354+sanyam-jn@users.noreply.github.com>
2020-07-04 15:15:44 +0530

    A basic xylophone app with Flutter
    it is a basic flutter app which has 7 keys and produces different sounds on touching those keys or that area on the screen (can be said as a lil xylophone on our mobiles)

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+
+                  color: Colors.red,
+                  onPressed: (){
+                    final player = AudioCache();
+                    player..play('note1.wav');
+                  },
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.orange,
+                  onPressed: (){
+                    final player = AudioCache();
+                    player..play('note2.wav');
+                  },
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.yellow,
+                  onPressed: (){
+                    final player = AudioCache();
+                    player..play('note3.wav');
+                  },
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.green,
+                  onPressed: (){
+                    final player = AudioCache();
+                    player..play('note4.wav');
+                  },
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.teal,
+                  onPressed: (){
+                    final player = AudioCache();
+                    player..play('note5.wav');
+                  },
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blue,
+                  onPressed: (){
+                    final player = AudioCache();
+                    player..play('note6.wav');
+                  },
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.purple,
+                  onPressed: (){
+                    final player = AudioCache();
+                    player..play('note7.wav');
+                  },
+                ),
+              ),
+
+            ],
+
+
+          ),
+        ),
+      ),
+    );
+  }
+}
